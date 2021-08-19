393dcf14cf5e6bb88d18dff7e9d9df87dd965118
JJ Marquis <76982850+JJMrqs@users.noreply.github.com>
2021-08-19 13:37:35 -0600

    edit connect.sh

--- connect.sh
+++ connect.sh
@@ -10,3 +10,4 @@ connect.sh
         scp -i SDC.pem ./init.sh ec2-user@ec2-18-218-193-54.us-east-2.compute.amazonaws.com:~init.sh
         ssh -i SDC.pem ec2-user@ec2-18-218-193-54.us-east-2.compute.amazonaws.com
         ;;
+esac
