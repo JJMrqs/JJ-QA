4be494aedfdbc4f21ac90eafbee8d8b95742e573
JJ Marquis <76982850+JJMrqs@users.noreply.github.com>
2021-08-21 14:35:38 -0600

    final nginx tweak

--- connect.sh
+++ connect.sh
@@ -7,12 +7,13 @@
         ;;
 
     node)
-        # scp -i SDC.pem ./init.sh ec2-user@ec2-18-191-147-180.us-east-2.compute.amazonaws.com:~/init.sh
-        ssh -i SDC.pem ec2-user@ec2-18-191-147-180.us-east-2.compute.amazonaws.com
+        scp -i SDC.pem ./init.sh ec2-user@ec2-3-137-181-24.us-east-2.compute.amazonaws.com:~/init.sh
+        ssh -i SDC.pem ec2-user@ec2-3-137-181-24.us-east-2.compute.amazonaws.com
         ;;
 
     lb)
         scp -i SDC.pem ./initLB.sh ec2-user@ec2-3-143-220-239.us-east-2.compute.amazonaws.com:~/initLB.sh
         ssh -i SDC.pem ec2-user@ec2-3-143-220-239.us-east-2.compute.amazonaws.com
         ;;
+
 esac
